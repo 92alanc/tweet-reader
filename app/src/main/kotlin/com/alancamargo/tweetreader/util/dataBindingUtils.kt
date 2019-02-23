44a8b6066933543c6987de4c74ded98941ce3a93
Alan Camargo <alan.uk.br@gmail.com>
2019-02-23 16:26:53 -0300

    Implementing main activity layout

---
+++ app/src/main/kotlin/com/alancamargo/tweetreader/util/dataBindingUtils.kt
@@ -0,0 +1,15 @@
+package com.alancamargo.tweetreader.util
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("imageUrl")
+fun setImageUrl(imageView: ImageView, url: String) {
+    // TODO
+}
+
+@BindingAdapter("timestamp")
+fun setTimestamp(textView: TextView, timestamp: String) {
+    // TODO
+}
