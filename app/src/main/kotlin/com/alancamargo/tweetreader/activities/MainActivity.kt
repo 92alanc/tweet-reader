feaadedb8d1cdae40e607ec8580674d45bd59b5d
Alan Camargo <alan.uk.br@gmail.com>
2019-02-25 08:53:04 -0300

    Implement main activity's structure

--- app/src/main/kotlin/com/alancamargo/tweetreader/activities/MainActivity.kt
+++ app/src/main/kotlin/com/alancamargo/tweetreader/activities/MainActivity.kt
@@ -2,13 +2,51 @@ package com.alancamargo.tweetreader.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.alancamargo.tweetreader.R
+import com.alancamargo.tweetreader.adapter.TweetAdapter
+import com.alancamargo.tweetreader.databinding.ActivityMainBinding
+import com.alancamargo.tweetreader.model.Tweet
+import com.alancamargo.tweetreader.model.User
+import com.alancamargo.tweetreader.repository.TwitterCallback
+import com.alancamargo.tweetreader.viewmodel.TweetViewModel
+import com.alancamargo.tweetreader.viewmodel.UserViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TwitterCallback {
+
+    private val adapter = TweetAdapter()
+
+    private val tweetViewModel by lazy {
+        ViewModelProviders.of(this).get(TweetViewModel::class.java)
+    }
+
+    private val userViewModel by lazy {
+        ViewModelProviders.of(this).get(UserViewModel::class.java)
+    }
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        userViewModel.getUserDetails(callback = this)
+        tweetViewModel.getTweets(callback = this)
+    }
+
+    override fun onUserDetailsFound(userDetails: LiveData<User>) {
+        userDetails.observe(this, Observer {
+            binding.user = it
+            binding.executePendingBindings()
+        })
+    }
+
+    override fun onTweetsFound(tweets: LiveData<List<Tweet>>) {
+        tweets.observe(this, Observer {
+            adapter.submitList(it)
+        })
     }
 
 }
