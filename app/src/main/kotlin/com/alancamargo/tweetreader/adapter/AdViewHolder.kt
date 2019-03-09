8d6adfcf6600a214686ae54eb1c884a6a8713336
Alan Camargo <alan.uk.br@gmail.com>
2019-03-09 16:53:23 -0300

    Remove ad recycler view items

--- app/src/main/kotlin/com/alancamargo/tweetreader/adapter/AdViewHolder.kt
+++
@@ -1,6 +0,0 @@
-package com.alancamargo.tweetreader.adapter
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-class AdViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
--- app/src/main/kotlin/com/alancamargo/tweetreader/adapter/TweetAdapter.kt
+++ app/src/main/kotlin/com/alancamargo/tweetreader/adapter/TweetAdapter.kt
@@ -5,41 +5,25 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import com.alancamargo.tweetreader.R
 import com.alancamargo.tweetreader.databinding.ItemTweetBinding
 import com.alancamargo.tweetreader.model.Tweet
 
-class TweetAdapter : ListAdapter<Tweet, RecyclerView.ViewHolder>(DiffCallback) {
+class TweetAdapter : ListAdapter<Tweet, TweetViewHolder>(DiffCallback) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TweetViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        return if (viewType == VIEW_TYPE_TWEET) {
-            val binding = DataBindingUtil.inflate<ItemTweetBinding>(inflater, R.layout.item_tweet,
-                parent, false)
-            TweetViewHolder(binding)
-        } else {
-            inflater.inflate(R.layout.item_ad, parent, false).let(::AdViewHolder)
-        }
+        val binding = DataBindingUtil.inflate<ItemTweetBinding>(inflater, R.layout.item_tweet,
+            parent, false)
+        return TweetViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (!isAdPosition(position)) {
-            val tweet = getItem(position)
-            (holder as TweetViewHolder).bindTo(tweet)
-        }
+    override fun onBindViewHolder(holder: TweetViewHolder, position: Int) {
+        val tweet = getItem(position)
+        holder.bindTo(tweet)
     }
-
-    override fun getItemViewType(position: Int): Int {
-        return if (isAdPosition(position)) VIEW_TYPE_AD else VIEW_TYPE_TWEET
-    }
-
-    private fun isAdPosition(position: Int) = (position != 0) && ((position + 1) % 10 == 0)
 
     companion object DiffCallback : DiffUtil.ItemCallback<Tweet>() {
-        private const val VIEW_TYPE_TWEET = 0
-        private const val VIEW_TYPE_AD = 1
-
         override fun areItemsTheSame(oldItem: Tweet, newItem: Tweet): Boolean {
             return oldItem.id == newItem.id
         }
