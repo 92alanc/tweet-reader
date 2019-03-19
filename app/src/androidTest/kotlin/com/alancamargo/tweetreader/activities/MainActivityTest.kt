8d4d186207e7c51047868f92b4ddc373adc67772
Alan Camargo <alan.uk.br@gmail.com>
2019-03-18 21:25:00 -0300

    Still trying to write those damn tests

--- app/src/androidTest/kotlin/com/alancamargo/tweetreader/activities/MainActivityTest.kt
+++ app/src/androidTest/kotlin/com/alancamargo/tweetreader/activities/MainActivityTest.kt
@@ -1,14 +1,19 @@
 package com.alancamargo.tweetreader.activities
 
+import com.alancamargo.tweetreader.api.CODE_FORBIDDEN
 import com.alancamargo.tweetreader.base.BaseActivityTest
 import com.alancamargo.tweetreader.robots.mainActivity
 import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.SocketPolicy
 import org.junit.Test
 
 class MainActivityTest : BaseActivityTest<MainActivity>(MainActivity::class) {
 
     @Test
     fun whenClickOnAuthorName_shouldRedirectToProfileActivity() {
+        mockTokenResponse()
+        mockTweetsResponse()
+
         mainActivity {
             onTweetAtPosition(0)
         } clickAuthorNameInTweet {
@@ -18,6 +23,9 @@ class MainActivityTest : BaseActivityTest<MainActivity>(MainActivity::class) {
 
     @Test
     fun whenClickOnAuthorScreenName_shouldRedirectToProfileActivity() {
+        mockTokenResponse()
+        mockTweetsResponse()
+
         mainActivity {
             onTweetAtPosition(0)
         } clickAuthorScreenNameInTweet {
@@ -27,6 +35,9 @@ class MainActivityTest : BaseActivityTest<MainActivity>(MainActivity::class) {
 
     @Test
     fun whenClickOnAuthorProfilePicture_shouldRedirectToProfileActivity() {
+        mockTokenResponse()
+        mockTweetsResponse()
+
         mainActivity {
             onTweetAtPosition(0)
         } clickProfilePictureInTweet {
@@ -34,19 +45,54 @@ class MainActivityTest : BaseActivityTest<MainActivity>(MainActivity::class) {
         }
     }
 
+    @Test
+    fun whenAccountIsSuspended_shouldDisplayMessage() {
+        mockTokenResponse()
+        mockAccountSuspendedResponse()
+
+        mainActivity {
+        } assert {
+            accountSuspendedMessageIsDisplayed()
+        }
+    }
+
+    @Test
+    fun whenAccountIsSuspended_shouldNotDisplayTweetList() {
+        mockTokenResponse()
+        mockAccountSuspendedResponse()
+
+        mainActivity {
+        } assert {
+            tweetListIsNotDisplayed()
+        }
+    }
+
+    private fun mockTokenResponse() {
+        val body = getJsonFromAsset("token.json")
+        enqueueResponseBody(body, 200)
+    }
+
     private fun mockTweetsResponse() {
         val body = getJsonFromAsset("tweets.json")
-        api.enqueue(MockResponse().setBody(body).setResponseCode(200))
+        enqueueResponseBody(body, 200)
     }
 
     private fun mockProfileDetailsResponse() {
         val body = getJsonFromAsset("profile_details.json")
-        api.enqueue(MockResponse().setBody(body).setResponseCode(200))
+        enqueueResponseBody(body, 200)
     }
 
     private fun mockAccountSuspendedResponse() {
         val body = getJsonFromAsset("account_suspended.json")
-        api.enqueue(MockResponse().setBody(body).setResponseCode(403))
+        enqueueResponseBody(body, CODE_FORBIDDEN)
+    }
+
+    private fun enqueueResponseBody(body: String, code: Int) {
+        api.enqueue(
+            MockResponse().setBody(body)
+                .setResponseCode(code)
+                .setSocketPolicy(SocketPolicy.KEEP_OPEN)
+        )
     }
 
 }
--- app/src/androidTest/kotlin/com/alancamargo/tweetreader/robots/mainActivityRobot.kt
+++ app/src/androidTest/kotlin/com/alancamargo/tweetreader/robots/mainActivityRobot.kt
@@ -1,6 +1,8 @@
 package com.alancamargo.tweetreader.robots
 
 import androidx.annotation.IdRes
+import br.com.concretesolutions.kappuccino.assertions.VisibilityAssertions.displayed
+import br.com.concretesolutions.kappuccino.assertions.VisibilityAssertions.notDisplayed
 import br.com.concretesolutions.kappuccino.custom.intent.IntentMatcherInteractions.sentIntent
 import br.com.concretesolutions.kappuccino.custom.recyclerView.RecyclerViewInteractions.recyclerView
 import com.alancamargo.tweetreader.R
@@ -18,17 +20,21 @@ class MainActivityRobot {
 
     infix fun clickProfilePictureInTweet(func: MainActivityResult.() -> Unit) {
         clickRecyclerViewChild(R.id.img_profile_picture)
-        applyResult(func)
+        assert(func)
     }
 
     infix fun clickAuthorNameInTweet(func: MainActivityResult.() -> Unit) {
         clickRecyclerViewChild(R.id.txt_name)
-        applyResult(func)
+        assert(func)
     }
 
     infix fun clickAuthorScreenNameInTweet(func: MainActivityResult.() -> Unit) {
         clickRecyclerViewChild(R.id.txt_screen_name)
-        applyResult(func)
+        assert(func)
+    }
+
+    infix fun assert(func: MainActivityResult.() -> Unit) {
+        MainActivityResult().run(func)
     }
 
     private fun clickRecyclerViewChild(@IdRes viewId: Int) {
@@ -40,10 +46,6 @@ class MainActivityRobot {
         }
     }
 
-    private fun applyResult(func: MainActivityResult.() -> Unit) {
-        MainActivityResult().run(func)
-    }
-
 }
 
 class MainActivityResult {
@@ -51,6 +53,18 @@ class MainActivityResult {
     fun redirectToProfileActivity() {
         sentIntent {
             className(ProfileActivity::class.java.name)
+        }
+    }
+
+    fun accountSuspendedMessageIsDisplayed() {
+        displayed {
+            id(R.id.txt_account_suspended)
+        }
+    }
+
+    fun tweetListIsNotDisplayed() {
+        notDisplayed {
+            id(R.id.recycler_view)
         }
     }
 
