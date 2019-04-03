90c467ec557c0a4be488997e01a99cc85ae8facb
Alan Camargo <alan.uk.br@gmail.com>
2019-04-03 18:59:30 -0300

    Fix date formatting

--- app/src/main/kotlin/com/alancamargo/tweetreader/util/dateTimeUtils.kt
+++ app/src/main/kotlin/com/alancamargo/tweetreader/util/dateTimeUtils.kt
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 fun formatDate(dateString: String): String {
-    val locale = Locale.getDefault()
+    val locale = Locale.ENGLISH
     val time = SimpleDateFormat("EEE MMM dd HH:mm:ss ZZZZZ yyyy", locale)
         .parse(dateString).time
     return SimpleDateFormat("dd/MM/yyyy HH:mm", locale).format(time)
