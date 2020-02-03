976300814207a0035071d7eb8c39853a00c78b3e
Alan Camargo <alan.uk.br@gmail.com>
2020-02-03 21:47:38 +0000

    Set OAuth2Token as data class

--- app/src/main/kotlin/com/alancamargo/tweetreader/model/api/OAuth2Token.kt
+++ app/src/main/kotlin/com/alancamargo/tweetreader/model/api/OAuth2Token.kt
@@ -2,8 +2,10 @@ package com.alancamargo.tweetreader.model.api
 
 import com.google.gson.annotations.SerializedName
 
-class OAuth2Token(@SerializedName("token_type") private val tokenType: String,
-                  @SerializedName("access_token") private val accessToken: String) {
+data class OAuth2Token(
+    @SerializedName("token_type") private val tokenType: String,
+    @SerializedName("access_token") private val accessToken: String
+) {
 
     fun getAuthorisationHeader() = "$tokenType $accessToken"
 
