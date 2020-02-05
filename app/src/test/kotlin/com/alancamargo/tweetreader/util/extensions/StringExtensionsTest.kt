2c6aa18e90aacef128b825a8367ffa027c0f8c21
Alan Camargo <alan.uk.br@gmail.com>
2020-02-05 19:18:03 +0000

    Replace Hamcrest with Truth

--- app/src/test/kotlin/com/alancamargo/tweetreader/util/extensions/StringExtensionsTest.kt
+++ app/src/test/kotlin/com/alancamargo/tweetreader/util/extensions/StringExtensionsTest.kt
@@ -1,7 +1,6 @@
 package com.alancamargo.tweetreader.util.extensions
 
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.MatcherAssert.assertThat
+import com.google.common.truth.Truth.assertThat
 import org.junit.Ignore
 import org.junit.Test
 
@@ -9,62 +8,62 @@ class StringExtensionsTest {
 
     @Test
     fun shouldSplitWords() {
-        val string = "This is a test string containing words, punctuation and " +
-                "numbers123."
+        val string = "This is a test string containing words, punctuation and numbers123."
         val expected = listOf("This", "is", "a", "test", "string", "containing",
             "words", "punctuation", "and", "numbers123.")
         val actual = string.getWords()
 
-        assertThat(actual, `is`(expected))
+
+        assertThat(actual).isEqualTo(expected)
     }
 
     @Test
     fun shouldDetectHashtagWithoutNumbers() {
         val string = "#Test"
 
-        assertThat(string.isHashtag(), `is`(true))
+        assertThat(string.isHashtag()).isTrue()
     }
 
     @Test
     fun shouldDetectHashtagWithNumbers() {
         val string = "#Test123"
 
-        assertThat(string.isHashtag(), `is`(true))
+        assertThat(string.isHashtag()).isTrue()
     }
 
     @Test
     fun shouldDetectHashtagWithUnderscores() {
         val string = "#Test_1_2_3"
 
-        assertThat(string.isHashtag(), `is`(true))
+        assertThat(string.isHashtag()).isTrue()
     }
 
     @Test
     fun shouldDetectMention() {
         val string = "@prisonplanet"
 
-        assertThat(string.isMention(), `is`(true))
+        assertThat(string.isMention()).isTrue()
     }
 
     @Test
     fun shouldDetectUrlWithHttpsWwwAndEndpoint() {
         val string = "http://www.github.com/alancamargo92"
 
-        assertThat(string.isUrl(), `is`(true))
+        assertThat(string.isUrl()).isTrue()
     }
 
     @Test
     fun shouldDetectUrlWithHttpsAndWww() {
         val string = "https://www.github.com"
 
-        assertThat(string.isUrl(), `is`(true))
+        assertThat(string.isUrl()).isTrue()
     }
 
     @Test
     fun shouldDetectUrlWithHttps() {
         val string = "https://github.com"
 
-        assertThat(string.isUrl(), `is`(true))
+        assertThat(string.isUrl()).isTrue()
     }
 
     @Test
@@ -73,7 +72,7 @@ class StringExtensionsTest {
     fun shouldDetectUrlWithDomainOnly() {
         val string = "github.com"
 
-        assertThat(string.isUrl(), `is`(true))
+        assertThat(string.isUrl()).isTrue()
     }
 
     @Test
@@ -82,35 +81,35 @@ class StringExtensionsTest {
     fun shouldDetectUrlWithDomainAndEndpoint() {
         val string = "github.com/alancamargo92"
 
-        assertThat(string.isUrl(), `is`(true))
+        assertThat(string.isUrl()).isTrue()
     }
 
     @Test
     fun shouldDetectUrlWithHttpWwwAndEndpoint() {
         val string = "http://www.github.com/alancamargo92"
 
-        assertThat(string.isUrl(), `is`(true))
+        assertThat(string.isUrl()).isTrue()
     }
 
     @Test
     fun shouldDetectUrlWithHttpAndWww() {
         val string = "http://www.github.com"
 
-        assertThat(string.isUrl(), `is`(true))
+        assertThat(string.isUrl()).isTrue()
     }
 
     @Test
     fun shouldDetectUrlWithHttp() {
         val string = "http://github.com"
 
-        assertThat(string.isUrl(), `is`(true))
+        assertThat(string.isUrl()).isTrue()
     }
 
     @Test
     fun shouldDetectPlainText() {
         val string = "This is a plain text with 8 words."
 
-        assertThat(string.isPlainText(), `is`(true))
+        assertThat(string.isPlainText()).isTrue()
     }
 
     @Test
@@ -118,21 +117,21 @@ class StringExtensionsTest {
     fun shouldDetectStringWithLink() {
         val string = "Check out my github on github.com/alancamargo92, it\'s amazing!"
 
-        assertThat(string.hasLink(), `is`(true))
+        assertThat(string.hasLink()).isTrue()
     }
 
     @Test
     fun shouldDetectShortUrl() {
         val string = "https://t.co/h12345ABc2y"
 
-        assertThat(string.hasLink(), `is`(true))
+        assertThat(string.hasLink()).isTrue()
     }
 
     @Test
     fun shouldNotSplitLinksWhenGettingSeparateWords() {
         val string = "https://t.co/h12345ABc2y"
 
-        assertThat(string.getWords().size, `is`(1))
+        assertThat(string.getWords().size).isEqualTo(1)
     }
 
 }
