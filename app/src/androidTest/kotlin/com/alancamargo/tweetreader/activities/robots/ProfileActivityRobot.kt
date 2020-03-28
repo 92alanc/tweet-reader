c542bce5aff9dc5122887ec0748e9857629da34f
Alan Camargo <alan.uk.br@gmail.com>
2020-03-28 15:41:55 +0000

    Writing UI tests for profile activity

--- app/src/androidTest/kotlin/com/alancamargo/tweetreader/activities/robots/ProfileActivityRobot.kt
+++ app/src/androidTest/kotlin/com/alancamargo/tweetreader/activities/robots/ProfileActivityRobot.kt
@@ -1,2 +1,53 @@
 package com.alancamargo.tweetreader.activities.robots
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.platform.app.InstrumentationRegistry
+import com.alancamargo.tweetreader.activities.ProfileActivity
+import com.alancamargo.tweetreader.activities.ProfileActivityTest
+import com.alancamargo.tweetreader.util.device.ConnectivityLiveData
+import io.mockk.every
+
+fun ProfileActivityTest.launchConnected(
+    block: ProfileActivityRobot.() -> Unit
+): ProfileActivityRobot {
+    isConnected(true)
+    return launch(block)
+}
+
+fun ProfileActivityTest.launchDisconnected(
+    block: ProfileActivityRobot.() -> Unit
+): ProfileActivityRobot {
+    isConnected(false)
+    return launch(block)
+}
+
+private fun ProfileActivityTest.launch(
+    block: ProfileActivityRobot.() -> Unit
+): ProfileActivityRobot {
+    every {
+        mockDeviceManager.getConnectivityState()
+    } returns ConnectivityLiveData(mockConnectivityHelper)
+
+    val context = InstrumentationRegistry.getInstrumentation().targetContext
+    
+    val intent = ProfileActivity.getIntent(context)
+    ActivityScenario.launch<ProfileActivity>(intent)
+
+    return ProfileActivityRobot().apply(block)
+}
+
+private fun ProfileActivityTest.isConnected(isConnected: Boolean) {
+    every { mockConnectivityHelper.isNetworkAvailable() } returns isConnected
+}
+
+class ProfileActivityRobot {
+
+
+
+}
+
+class ProfileActivityAssertions {
+
+
+
+}
